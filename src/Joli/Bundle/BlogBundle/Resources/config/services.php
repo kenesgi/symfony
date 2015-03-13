19dbb4597b2bc3fda234bc3f121204f5d358bc05
kenesgi <kenda10@hotmail.fr>
2015-03-13 08:51:58 +0100

    Premier commit

---
+++ src/Joli/Bundle/BlogBundle/Resources/config/services.php
@@ -0,0 +1,21 @@
+<?php
+
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Parameter;
+
+/*
+
+$container->setDefinition(
+    'joli_blog.example',
+    new Definition(
+        'Joli\Bundle\BlogBundle\Example',
+        array(
+            new Reference('service_id'),
+            "plain_value",
+            new Parameter('parameter_name'),
+        )
+    )
+);
+
+*/
---
+++ src/PB/BlogBundle/Controller/ArticleController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace PB\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ArticleController extends Controller
+{
+    public function indexAction($page)
+    {
+    	if ($page < 1) {
+      		throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+    	}
+
+    	$listArticles = array(
+      array(
+        'title'   => 'Recherche développpeur Symfony2',
+        'id'      => 1,
+        'author'  => 'Alexandre',
+        'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'Mission de webmaster',
+        'id'      => 2,
+        'author'  => 'Hugo',
+        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'Offre de stage webdesigner',
+        'id'      => 3,
+        'author'  => 'Mathieu',
+        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+        'date'    => new \Datetime())
+    );
+
+    // Et modifiez le 2nd argument pour injecter notre liste
+    return $this->render('PBBlogBundle:Article:index.html.twig', array(
+      'listArticles' => $listArticles
+    ));
+    }
+    
+  	public function viewAction($id)
+  	{
+    
+    	$article = array(
+      		'title'   => 'Recherche développpeur Symfony2',
+   			'id'      => $id,
+   			'author'  => 'Alexandre',
+      		'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+      		'date'    => new \Datetime()
+    	);
+
+    return $this->render('PBBlogBundle:Article:view.html.twig', array(
+      'article' => $article
+    ));
+  	}
+  	
+  	public function addAction(Request $request)
+
+  {
+
+    if ($request->isMethod('POST')) {
+
+
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+
+      return $this->redirect($this->generateUrl('pb_blog_view', array('id' => 5)));
+
+    }
+
+
+    return $this->render('PBBlogBundle:Article:add.html.twig');
+
+  }
+
+  public function editAction($id, Request $request)
+
+  {
+
+    if ($request->isMethod('POST')) {
+
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+
+      return $this->redirect($this->generateUrl('pb_blog_view', array('id' => 5)));
+
+    }
+
+    return $this->render('PBBlogBundle:Article:edit.html.twig', array('article' => $article));
+
+  }
+
+  public function deleteAction($id)
+
+  {
+
+    return $this->render('PBBlogBundle:Article:delete.html.twig');
+
+  }
+  
+  public function menuAction($limit)
+
+  {
+
+    // On fixe en dur une liste ici, bien entendu par la suite
+
+    // on la récupérera depuis la BDD !
+
+    $listArticles = array(
+
+      array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
+
+      array('id' => 5, 'title' => 'Mission de webmaster'),
+
+      array('id' => 9, 'title' => 'Offre de stage webdesigner')
+
+    );
+
+    return $this->render('PBBlogBundle:Article:menu.html.twig', array(
+
+      // Tout l'intérêt est ici : le contrôleur passe
+
+      // les variables nécessaires au template !
+
+      'listArticles' => $listArticles
+
+    ));
+
+  }
+  
+}
